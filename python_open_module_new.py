f7fe78627fe67fe1918d779d8e99bee4782a13af
Michal Danilak <mimino.coder@gmail.com>
2021-03-31 11:08:12 +0200

    Fixed for ST3 and Python 3

--- python_open_module_new.py
+++ python_open_module_new.py
@@ -1,22 +1,22 @@
 from fnmatch import fnmatch
+from os import path
 import imp
 import os
-import os.path as path
 import re
-import sublime
-import sublime_plugin
 import subprocess
 import sys
 import threading
 import time
 
+import sublime
+import sublime_plugin
+
 
 SETTINGS_FILE = 'Python Open Module (New).sublime-settings'
-settings = sublime.load_settings(SETTINGS_FILE)
-prefs = sublime.load_settings('Preferences.sublime-settings')
 
 SYNTAX_FILENAME = path.join('Packages', 'Python Open Module (New)', 'Python Module Path.tmLanguage')
 COLOR_SCHEME_FILENAME = path.join('Packages', 'Python Open Module (New)', 'Python Module Path.stTheme')
+
 
 si = None
 if hasattr(subprocess, 'STARTUPINFO'):
@@ -27,13 +27,22 @@ if hasattr(subprocess, 'STARTUPINFO'):
 
 def debug(*args):
     LOCAL_DIR = path.dirname(path.abspath(__file__))
-    with open(path.join(LOCAL_DIR, 'debug.txt'), 'ab') as f:
+    with open(path.join(LOCAL_DIR, 'debug.txt'), 'a') as f:
         f.write('%(line)s\n%(obj)s\n%(line)s\n\n' %
                 {'line': '=' * 40, 'obj': ' '.join(map(str, args))})
 
 
+def get_settings():
+    return sublime.load_settings(SETTINGS_FILE)
+
+
+def get_preferences():
+    return sublime.load_settings('Preferences.sublime-settings')
+
+
 class ProjectPackagesManager(object):
-    '''ProjectPackagesManager discovers and maintains the list of top-level
+    '''
+    `ProjectPackagesManager` discovers and maintains the list of top-level
     Python packages in the current project.
     '''
 
@@ -47,47 +56,44 @@ class ProjectPackagesManager(object):
         if not self.running and cur_time - self.last_run >= 60:
             self.running = True
             self.last_run = cur_time
-            threading.Thread(target=self._find_project_packages, kwargs={
+            threading.Thread(target=self._discover_project_packages, kwargs={
                 'root_folders': window.folders(),
-                'folder_exclude_patterns': prefs.get(
-                    'folder_exclude_patterns', []),
-                'python_extensions': settings.get(
-                    'python_extensions', ['.py']),
+                'folder_exclude_patterns': get_preferences().get('folder_exclude_patterns', []),
+                'python_extensions': get_settings().get('python_extensions', ['.py']),
             }).start()
 
-    def _find_project_packages(self, root_folders, folder_exclude_patterns,
-                               python_extensions):
-        exclude_folders = [path.join('*', x) for x in folder_exclude_patterns]
-        init_file_pattern = re.compile(
-            '__init__(%s)' % '|'.join(python_extensions).replace('.', r'\.'))
+    def _discover_project_packages(self, root_folders, folder_exclude_patterns, python_extensions):
+        exclude_folders = [path.join('*', one) for one in folder_exclude_patterns]
+        init_filename_pattern = re.compile('__init__(%s)' % '|'.join(python_extensions).replace('.', r'\.'))
         project_packages = set()
 
-        def is_ok(dir_path):
-            return all(not fnmatch(dir_path, x) for x in exclude_folders)
+        def is_ok(dirpath):
+            return all(not fnmatch(dirpath, one) for one in exclude_folders)
 
-        def is_package(files):
-            return any(init_file_pattern.match(x) for x in files)
+        def is_package(filenames):
+            return any(init_filename_pattern.match(filename) for filename in filenames)
 
         for root_folder in root_folders:
-            for root, folders, files in os.walk(root_folder):
-                if is_package(files):
-                    project_packages.add(path.dirname(root))
-                    folders[:] = []
+            for dirpath, dirnames, filenames in os.walk(root_folder):
+                if is_package(filenames):
+                    project_packages.add(path.dirname(dirpath))
+                    dirnames[:] = []
                 else:
-                    folders[:] = filter(lambda x: is_ok(path.join(root, x)),
-                                        folders)
+                    dirnames[:] = filter(lambda dirname: is_ok(path.join(dirpath, dirname)),
+                                         dirnames)
+
         self.packages = list(project_packages)
         self.running = False
 
 
 class PythonOpenModuleNewCommand(sublime_plugin.WindowCommand):
-
     def __init__(self, *args, **kwargs):
         super(PythonOpenModuleNewCommand, self).__init__(*args, **kwargs)
         self.project_packages = ProjectPackagesManager()
 
     def run(self):
         self.project_packages.refresh(self.window)
+
         view = self.window.active_view()
         text = '' if view is None else view.substr(view.sel()[0])
         panel_view = self.window.show_input_panel(
@@ -97,17 +103,20 @@ class PythonOpenModuleNewCommand(sublime_plugin.WindowCommand):
         panel_view.settings().set('syntax', SYNTAX_FILENAME)
         panel_view.settings().set('color_scheme', COLOR_SCHEME_FILENAME)
 
-    def on_done(self, input):
-        match = re.match(r'^\s*(?P<new_window>\+?)\s*(?P<dots>\.*)'
-                         r'(?P<absolute_path>(?:\w+)?(?:\.\w+)*)\s*$', input)
+    def on_done(self, text):
+        match = re.match(
+            r'^\s*(?P<new_window>\+?)\s*(?P<dots>\.*)'
+            r'(?P<absolute_path>(?:\w+)?(?:\.\w+)*)\s*$',
+            text)
+
         if not match:
-            sublime.status_message('Invalid python module path: `%s`' % input)
+            sublime.status_message('Invalid python module path: `%s`' % text)
             return
 
         open_new_window = match.group('new_window')
         dots = match.group('dots')
         absolute_path = match.group('absolute_path')
-        input = '%s%s' % (dots, absolute_path)
+        text = '%s%s' % (dots, absolute_path)
 
         if dots:
             filename = self._get_relative_module_filename(dots, absolute_path)
@@ -117,9 +126,9 @@ class PythonOpenModuleNewCommand(sublime_plugin.WindowCommand):
             return
 
         if filename is None:
-            sublime.status_message('Module `%s` not found' % input)
+            sublime.status_message('Module `%s` not found' % text)
         else:
-            sublime.status_message('Module `%s` found: %s' % (input, filename))
+            sublime.status_message('Module `%s` found: %s' % (text, filename))
 
             if open_new_window:
                 self._open_new_window(filename)
@@ -134,6 +143,7 @@ class PythonOpenModuleNewCommand(sublime_plugin.WindowCommand):
             subl_command = 'sublime_text'
         else:
             subl_command = 'subl'
+
         try:
             # for packages, open the whole directory
             if path.splitext(path.basename(filename))[0] == '__init__':
@@ -155,69 +165,31 @@ class PythonOpenModuleNewCommand(sublime_plugin.WindowCommand):
             folder = path.normcase(path.realpath(folder))
             if folder == filename:
                 return True
+
             folder += os.sep
             if path.commonprefix([folder, filename]) == folder:
                 return True
+
         return False
 
-    def _get_sys_path(self):
-        '''Return the sys path to be searched with user preferences (e.g. venv,
-        path setting) and the current project taken into account.
-
-        We cannot simply use sys.path inside the sublime script, because it is
-        modified by ST2. We need to ask external Python script for this.
-
-        Update: as a second return value, return the dictionary of already
-        imported modules (received from sys.modules). This solves problems
-        with some .pth scripts
-        (e.g. `zc` module from http://pypi.python.org/pypi/zc.queue)
+    def _get_python_script(self, dirpath, script_name):
         '''
-        python_script = (
-            'import sys;'
-            'print sys.path;'
-            'print dict((k, m.__path__[0]) for (k, m) in sys.modules.items() if hasattr(m, "__path__"));'
-        )
-
-        path_modifications = settings.get(
-            'path',
-            {'prepend': [], 'append': [], 'replace': []})
-        imported_modules = {}
-        if path_modifications.get('replace'):
-            result_path = path_modifications['replace']
-        else:
-            python_exec = settings.get('python_executable') or 'python'
-            try:
-                python = subprocess.Popen(
-                    [python_exec, '-u', '-c', python_script],
-                    stdout=subprocess.PIPE,
-                    startupinfo=si
-                )
-                result_path, imported_modules = map(eval, python.communicate()[0].decode('utf-8').split('\n')[:2])
-            except:
-                result_path = sys.path
-        return (
-            (
-                path_modifications.get('prepend', []) +
-                result_path +
-                path_modifications.get('append', []) +
-                self.project_packages.packages
-            ),
-            imported_modules)
-
-    def _get_python_script(self, dir_path, script_name):
-        '''Return the absolute path to the python script `script_name`
+        Return the absolute path to the python script `script_name`
         (given without an extension) which should be contained somewhere inside
-        `dir_path` directory.
+        `dirpath` directory.
         '''
-        for ext in settings.get('python_extensions', ['.py']):
-            filename = path.join(dir_path, script_name + ext)
+
+        for ext in get_settings().get('python_extensions', ['.py']):
+            filename = path.join(dirpath, script_name + ext)
             if path.exists(filename):
                 return path.abspath(filename)
 
     def _get_absolute_module_filename(self, absolute_path, start_path=None):
-        '''Return the absolute path to the python script from the given
+        '''
+        Return the absolute filepath to the python script from the given
         absolute module path.
         '''
+
         if start_path is None:
             sys_path, imported_modules = self._get_sys_path()
         else:
@@ -244,13 +216,64 @@ class PythonOpenModuleNewCommand(sublime_plugin.WindowCommand):
             pass
 
     def _get_relative_module_filename(self, dots, absolute_path):
-        '''Return the absolute path to the python script from the given
+        '''
+        Return the absolute path to the python script from the given
         relative module path. Relative path is relative to the working file.
         '''
+
         view = self.window.active_view()
         if view is None:
             return
+
         start_path = view.file_name()
-        for d in dots:
+        # move up in directory structure for each dot
+        for _ in dots:
             start_path = path.dirname(start_path)
+
         return self._get_absolute_module_filename(absolute_path, [start_path])
+
+    def _get_sys_path(self):
+        '''
+        Return the sys path to be searched with user preferences (e.g. venv,
+        path setting) and the current project taken into account.
+
+        We cannot simply use sys.path inside the sublime script, because it is
+        modified by ST. We need to ask external Python script for this.
+
+        Update: as a second return value, return the dictionary of already
+        imported modules (received from sys.modules). This solves problems
+        with some .pth scripts
+        (e.g. `zc` module from http://pypi.python.org/pypi/zc.queue)
+        '''
+
+        settings = get_settings()
+        path_modifications = settings.get(
+            'path',
+            {'prepend': [], 'append': [], 'replace': []})
+
+        imported_modules = {}
+        if path_modifications.get('replace'):
+            result_path = path_modifications['replace']
+        else:
+            python_exec = settings.get('python_executable') or 'python'
+            try:
+                python_script = (
+                    'import sys;'
+                    'print(sys.path);'
+                    'print(dict((k, m.__path__[0]) for (k, m) in sys.modules.items() if hasattr(m, "__path__") and isinstance(m.__path__, list)));')
+                python = subprocess.Popen(
+                    [python_exec, '-u', '-c', python_script],
+                    stdout=subprocess.PIPE,
+                    startupinfo=si)
+                result_path, imported_modules = map(eval, python.communicate()[0].decode('utf-8').split('\n')[:2])
+            except:
+                result_path = sys.path
+
+        return (
+            (
+                path_modifications.get('prepend', []) +
+                result_path +
+                path_modifications.get('append', []) +
+                self.project_packages.packages
+            ),
+            imported_modules)
